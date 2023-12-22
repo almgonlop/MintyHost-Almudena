7d725184e9813254b9b55c3ffa3774fda30943f2
almgonlop <almu.gon.12@gmail.com>
2023-12-22 13:51:15 +0100

    Filtros

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -8,14 +8,22 @@
     <FiltrosPanel @filtrarApartamentos="filtrarApartamentos" />
     <ApartmentList :apartamentos="apartamentosFiltrados" />
         <!-- Mostrar los apartamentos filtrados -->
-        <div v-if="apartamentosFiltrados.length > 0">
-      <h2>Apartamentos Filtrados:</h2>
-      <ul>
-        <li v-for="apartamento in apartamentosFiltrados" :key="apartamento.id">
-          {{ apartamento.apartment_title }} - {{ apartamento.address }}
-        </li>
-      </ul>
-    </div>
+      <div v-if="apartamentosFiltrados.length > 0">
+        <h2>Apartamentos Filtrados:</h2>
+        <ul>
+          <li v-for="apartamento in apartamentosFiltrados" :key="apartamento.id">
+            {{ apartamento.apartment_title }} - {{ apartamento.address }}
+          </li>
+        </ul>
+      </div>
+      <div v-else-if="sinResultados">
+  <p>No se encontraron apartamentos con los filtros seleccionados.</p>
+</div>
+
+<!-- Mostrar mensaje por defecto -->
+<div v-else>
+  <p>Selecciona un barrio y aplica filtros para buscar apartamentos.</p>
+</div>
   </div>
   </div>
 </template>
@@ -41,41 +49,40 @@ export default {
   data() {
     return {
       apartamentos: [], // Almacenar la lista de todos los apartamentos
-      apartamentosFiltrados: [] // Almacenar la lista de apartamentos después de aplicar filtros
+      apartamentosFiltrados: [], // Almacenar la lista de apartamentos después de aplicar filtros
+      sinResultados: false
     };
   },
   methods: {
-    filtrarApartamentos({ barrioSeleccionado, filtros }) {
-  console.log('Barrio Seleccionado en MainPage:', barrioSeleccionado);
-  console.log('Filtros Seleccionados en MainPage:', filtros);
+    async filtrarApartamentos({ barrioSeleccionado, filtros }) {
+      console.log('Barrio Seleccionado en MainPage:', barrioSeleccionado);
+      console.log('Filtros Seleccionados en MainPage:', filtros);
 
-  // Verifica que filtros sea un objeto válido antes de acceder a sus propiedades
-  if (filtros && typeof filtros === 'object') {
-    // Utiliza la información recibida para construir la solicitud a la API, por ejemplo:
-    const params = {
-      barrio_id: barrioSeleccionado,
-      bedrooms: filtros.numDormitorios,
-      guests: filtros.capacidadMaxima,
-      min_price: filtros.rangoPrecio && filtros.rangoPrecio.min,
-      max_price: filtros.rangoPrecio && filtros.rangoPrecio.max,
-    };
+      // Construir la solicitud a la API utilizando los filtros generales
+      const params = {
+        bedrooms: filtros.numDormitorios,
+        guests: filtros.capacidadMaxima,
+        min_price: filtros.rangoPrecio.min,
+        max_price: filtros.rangoPrecio.max,
+      };
 
-    console.log('Parámetros de búsqueda:', params);
+      try {
+        // Realizar la solicitud a la API para obtener los apartamentos según los filtros generales
+        const response = await axios.post('https://api.dev.myplazze.com/api/v1/practice/search', params);
+        const apartamentosFiltradosGenerales = response.data;
 
-    // Realiza la solicitud a la API utilizando Axios u otra biblioteca
-    axios.post('https://api.dev.myplazze.com/api/v1/practice/search', params)
-      .then(response => {
-        console.log('Respuesta de la búsqueda de apartamentos:', response.data);
-        // Almacena la respuesta en la propiedad de datos
-        this.apartamentosFiltrados = response.data;
-      })
-      .catch(error => {
+        // Filtrar los apartamentos obtenidos por el barrio seleccionado
+        const apartamentosFiltradosPorBarrio = apartamentosFiltradosGenerales.filter(apartamento => {
+          return apartamento.barrio.id === barrioSeleccionado.id;
+        });
+        // Almacenar la lista final de apartamentos filtrados
+        this.apartamentosFiltrados = apartamentosFiltradosPorBarrio;
+        // Verificar si no hay resultados
+        this.sinResultados = this.apartamentosFiltrados.length === 0;
+      } catch (error) {
         console.error('Error al realizar la búsqueda de apartamentos', error);
-      });
-  } else {
-    console.error('Filtros no válidos:', filtros);
-  }
-}
+      }
+    },
   },
 };
 </script>
